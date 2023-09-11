e7238b559acf5ad16f83ed714b9183e3f8e73aaa
tejaromalius <tayefi.ilia@protonmail.com>
2023-09-12 00:14:45 +0300

    Added test suits to validate recipes' integrity
    * Add pyproject.toml and poetry for package management
    * Add test_recipes.py test suit
    * Update .gitignore

---
+++ tests/test_recipes.py
@@ -0,0 +1,28 @@
+import os
+import pytest
+import subprocess
+
+
+@pytest.fixture
+def script_paths():
+    scripts_dir = "scripts/"
+    script_paths = {
+        basename: os.path.join(scripts_dir, basename, f"{basename}.sh")
+        for basename in os.listdir(scripts_dir)
+    }
+    return script_paths
+
+
+def test_compilation(script_paths):
+    for basename, script_path in script_paths.items():
+        process = subprocess.run(["shellcheck", "-e", "SC2148", script_path])
+        assert process.returncode == 0, f"`{basename}` failed to compile!"
+
+
+def test_execution(script_paths):
+    for basename, script_path in script_paths.items():
+        process = subprocess.run(
+            ["bash", "-c", "-i",
+                f"source {script_path}; {basename} > /dev/null"]
+        )
+        assert process.returncode == 0, f"`{basename}` failed to execute!"

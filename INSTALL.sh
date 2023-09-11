05aa0cb9a2893729b26a34011a3074117d55ae0e
tejaromalius <tayefi.ilia@protonmail.com>
2023-09-12 00:49:15 +0300

    Fix small bugs
    * Update INSTALLER.sh to append new line to .bashrc
    * Fix color printing in gstat.sh
    * Update .gitignore

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,7 +1,5 @@
 # read recipe's name
 recipe="$1";
-# go to the home directory
-cd ~;
 # check recipe's existance
 if grep -q -w "$recipe" ~/.bashrc; then
     echo -e "\033[0;33mrecipe exists in ~/.bashrc. \033[0;31maborting..\033[0m"
@@ -12,9 +10,9 @@ else
     if [ "$request_result" != "404: Not Found" ]; then
         # recipe exists in repo
         # append recipe to .bashrc
-        echo "$request_result" >> .bashrc;
+        echo >> ~/.bashrc; echo "$request_result" >> ~/.bashrc
         # source the .bashrc
-        source .bashrc;
+        source ~/.bashrc;
         echo -e "\033[0;32mrecipe installed! restart terminal and use by running '$recipe'! \033[0m"
     else
         echo -e "\033[0;33mrecipe not found in repo. \033[0;31maborting..\033[0m"

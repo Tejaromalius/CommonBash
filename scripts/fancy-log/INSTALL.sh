c937fb8688e8836a78126488405e3aad0e574653
tejaromalius <tayefi.ilia@protonmail.com>
2023-09-11 11:57:50 +0300

    Created installation safe-guard
    * Created a check to avoid duplications

--- scripts/fancy-log/INSTALL.sh
+++ scripts/fancy-log/INSTALL.sh
@@ -2,8 +2,14 @@
 recipe="fancy-log";
 # go to the home directory
 cd ~;
-# download code from github repo and append to .bashrc
-curl -sSL https://raw.githubusercontent.com/Tejaromalius/CommonBash/main/scripts/$recipe/$recipe.sh >> .bashrc;
-# source the .bashrc
-source .bashrc;
-# recipe installed! restart terminal and use by running 'fancy-log'!"
+# check recipe's existance
+if grep -q "$recipe" ~/.bashrc; then
+    echo -e "\033[0;33mrecipe exists in ~/.bashrc. \033[0;31maborting..\033[0m"
+else
+    # recipe doesn't exist
+    # download code from github repo and append to .bashrc
+    curl -sSL https://raw.githubusercontent.com/Tejaromalius/CommonBash/main/scripts/$recipe/$recipe.sh >> .bashrc;
+    # source the .bashrc
+    source .bashrc;
+    echo -e "\033[0;32mrecipe installed! restart terminal and use by running '$recipe'! \033[0m"
+fi

9c5f247d6900797515f62dbfb27ba7551b2b2ac0
tejaromalius <tayefi.ilia@protonmail.com>
2023-09-13 23:46:32 +0300

    Upgraded installer to install in .recipes
    * Added funtionality to INSTALL.sh to open and write updated recipes to
      .recipes file in /home directory

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,20 +1,42 @@
+#! /usr/bin/bash
+
+bashrc_path="$HOME/.bashrc"
+recipes_path="$HOME/.recipes"
+
+# check recipe's existance
+if ! grep -q -x "source $recipes_path" "$bashrc_path"; then
+    printf "\nsource %s" "$recipes_path" >> "$bashrc_path"
+fi
+
 # read recipe's name
 recipe="$1";
-# check recipe's existance
-if grep -q -w "$recipe" ~/.bashrc; then
-    echo -e "\033[0;33mrecipe exists in ~/.bashrc. \033[0;31maborting..\033[0m"
-else
-    # recipe doesn't exist
-    # download code from github repo
-    request_result="$(curl -sSL https://raw.githubusercontent.com/Tejaromalius/CommonBash/main/recipes/$recipe/$recipe.sh)";
-    if [ "$request_result" != "404: Not Found" ]; then
-        # recipe exists in repo
-        # append recipe to .bashrc
-        echo >> ~/.bashrc; echo "$request_result" >> ~/.bashrc
-        # source the .bashrc
-        source ~/.bashrc;
-        echo -e "\033[0;32mrecipe installed! restart terminal and use by running '$recipe'! \033[0m"
+
+# download code from github repo
+request_result="$(curl -sSL https://raw.githubusercontent.com/Tejaromalius/CommonBash/main/recipes/"$recipe"/"$recipe.sh")";
+if [ "$request_result" != "404: Not Found" ]; then
+    # recipe exists in repo
+    # check output file's existence
+    if [ ! -f "$recipes_path" ] > /dev/null; then
+        # output doesn't exist. creating new one
+        touch "$recipes_path"
     else
-        echo -e "\033[0;33mrecipe not found in repo. \033[0;31maborting..\033[0m"
+        # check recipe's existance
+        if grep -q -w "$recipe" "$recipes_path"; then
+            echo -e "\033[0;33m'$recipe' exists in ~/.recipes. \033[0;32mupdating..\033[0m"
+
+            start_marker="# >>> $recipe >>>"
+            end_marker="# <<< $recipe <<<"
+            
+            # remove the old recipe
+            sed -i "/$start_marker/,/$end_marker/d" "$recipes_path"
+        fi
     fi
+
+    # append recipe to .recipes file
+    printf "%s\n" "$request_result" >> "$recipes_path"
+    # source the .bashrc
+    source "$bashrc_path";
+    echo -e "\033[0;32mdone! restart terminal and run '$recipe'! \033[0m"
+else
+    echo -e "\033[0;33m'$recipe' not found in repo. \033[0;31maborting..\033[0m"
 fi

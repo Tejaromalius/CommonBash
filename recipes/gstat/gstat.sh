603dd6205f52fe21a9ac48417039c5ad1f4aaae0
tejaromalius <tayefi.ilia@protonmail.com>
2023-09-14 21:35:20 +0300

    Fixed & optimized `gstat` recipe's output

--- recipes/gstat/gstat.sh
+++ recipes/gstat/gstat.sh
@@ -14,29 +14,49 @@ gstat() {
   echo
 
   # check for changes to be committed
-  changes_to_commit=$(git status --porcelain=v1 | awk '/^[MDRCTU] / {print "\t./" $2}')
+  changes_to_commit=$(git status --short | awk '/^[MADRCU]/ {
+    color = "";
+    if ($1 ~ /^M/) color = "\033[31m";   # Red for modified
+    if ($1 ~ /^A/) color = "\033[32m";   # Green for added
+    if ($1 ~ /^D/) color = "\033[31m";   # Red for deleted
+    if ($1 ~ /^R/) color = "\033[33m";   # Yellow for renamed
+    if ($1 ~ /^C/) color = "\033[33m";   # Yellow for copied
+    if ($1 ~ /^U/) color = "\033[35m";   # Magenta for unmerged
+
+    print "\t" color $0 "\033[0m";       # Reset color
+}')
 
   # check for changes not staged for commit
-  changes_not_staged=$(git status --porcelain=v1 | awk '/^ [MDRCTU]/ {print "\t./" $2}')
+  changes_not_staged=$(git status --short | awk '/^.[MADRCU]/ {
+    color = "";
+    if ($1 ~ /^.?M/) color = "\033[31m";   # Red for modified
+    if ($1 ~ /^.?A/) color = "\033[32m";   # Green for added
+    if ($1 ~ /^.?D/) color = "\033[31m";   # Red for deleted
+    if ($1 ~ /^.?R/) color = "\033[33m";   # Yellow for renamed
+    if ($1 ~ /^.?C/) color = "\033[33m";   # Yellow for copied
+    if ($1 ~ /^.?U/) color = "\033[35m";   # Magenta for unmerged
+
+    print "\t" color $0 "\033[0m";       # Reset color
+}')
 
   # check for untracked files
-  untracked_files=$(git status --porcelain=v1 | awk '/^\?\?/ {print "\t./" $2}')
+  untracked_files=$(git status --porcelain=v1 | awk '/^\?\?/ {color = "\033[95m"; print "\t" color $0}')
 
   # display categories with non-empty content
   if [[ -n $changes_to_commit ]]; then
-    echo "$(tput bold setaf 2)Changes to be committed:$(tput sgr0)"
+    echo "$(tput bold setaf 253)Changes to be committed:$(tput sgr0)"
     echo "$changes_to_commit"
     echo
   fi
 
   if [[ -n $changes_not_staged ]]; then
-    echo "$(tput bold setaf 1)Changes not staged for commit:$(tput sgr0)"
+    echo "$(tput bold setaf 253)Changes not staged for commit:$(tput sgr0)"
     echo "$changes_not_staged"
     echo
   fi
 
   if [[ -n $untracked_files ]]; then
-    echo "$(tput setaf 3)Untracked files:$(tput sgr0)"
+    echo "$(tput bold setaf 253)Untracked files:$(tput sgr0)"
     echo "$untracked_files"
     echo
   fi

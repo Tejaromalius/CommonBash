bbb09697328ddb88f0e25248f33d4f987ee9b589
tejaromalius <tayefi.ilia@protonmail.com>
2023-09-16 09:22:05 +0300

    Added extra argument input for `glog`

--- recipes/glog/glog.sh
+++ recipes/glog/glog.sh
@@ -1,5 +1,5 @@
 # >>> glog >>>
 glog() {
-    git log --graph --abbrev-commit --decorate --pretty=oneline --format=format:'%C(auto)%C(dim white)%h %C(bold #6F2DC3)%an %C(auto)%d  %C(auto)%s %C(dim white)(%ad)%C(reset)' --date=format:'%Y/%m/%d %H:%M:%S'
+    git log --graph --abbrev-commit --decorate --pretty=oneline --format=format:'%C(auto)%C(dim white)%h %C(bold #6F2DC3)%an %C(auto)%d  %C(auto)%s %C(dim white)(%ad)%C(reset)' --date=format:'%Y/%m/%d %H:%M:%S' "$@"
 }
 # <<< glog <<<

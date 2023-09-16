29466e608d31c45ca26d1a2d10aaf482a8f4e7f0
tejaromalius <tayefi.ilia@protonmail.com>
2023-09-16 09:00:15 +0300

    Fixed small bugs in `exal` recipe

--- recipes/exal/exal.sh
+++ recipes/exal/exal.sh
@@ -3,7 +3,7 @@ exal() {
   # check if 'exa' is installed
   if command -v exa &>/dev/null; then
     # if 'exa' is installed, run the desired command
-    exa -l -a -L 2 -T -s name --group-directories-first "$@"
+    exa -l -a -L 2 -s name --group-directories-first "$@"
   else
     echo -e "\033[0;33m'exa' is not installed! please install it from 'https://github.com/ogham/exa#installation' and run again. \033[0;31maborting..\033[0m"
   fi

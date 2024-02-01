0dbb164d2c8ee926a059b946ce4a06bdb963689e
tejaromalius <tayefi.ilia@protonmail.com>
2024-02-01 21:04:00 +0300

    Added Safe Fail For `glog`

--- recipes/glog/glog.sh
+++ recipes/glog/glog.sh
@@ -1,5 +1,9 @@
 # >>> glog >>>
 glog() {
-    git log --graph --abbrev-commit --decorate --pretty=oneline --format=format:'%C(auto)%C(dim white)%h %C(bold #6F2DC3)%an %C(auto)%d  %C(auto)%s %C(dim white)(%ad)%C(reset)' --date=format:'%Y/%m/%d %H:%M:%S' "$@"
+  if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+    echo "$(tput setaf 1)Warning: Not a Git repository.$(tput sgr0)"
+    return 1
+  fi
+  git log --graph --abbrev-commit --decorate --pretty=oneline --format=format:'%C(auto)%C(dim white)%h %C(bold #6F2DC3)%an %C(auto)%d  %C(auto)%s %C(dim white)(%ad)%C(reset)' --date=format:'%Y/%m/%d %H:%M:%S' "$@"
 }
 # <<< glog <<<

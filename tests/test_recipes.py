05d7925f3a278d033b18b0d091d88ddf50390feb
tejaromalius <tayefi.ilia@protonmail.com>
2024-12-12 13:46:52 +0300

    revised test to suit the new recipes needs

--- tests/test_recipes.py
+++ tests/test_recipes.py
@@ -5,24 +5,47 @@ import subprocess
 
 @pytest.fixture
 def script_paths():
-    scripts_dir = "recipes/"
-    script_paths = {
-        basename: os.path.join(scripts_dir, basename, f"{basename}.sh")
-        for basename in os.listdir(scripts_dir)
-    }
-    return script_paths
+  scripts_dir = "recipes/"
+  script_paths = {
+    basename: os.path.join(scripts_dir, basename, f"{basename}.sh")
+    for basename in os.listdir(scripts_dir)
+  }
+  return script_paths
 
+@pytest.fixture
+def scripts_to_skip():
+  return ['snvim', 'venv', 'trash', 'lsps']
 
-def test_compilation(script_paths):
-    for basename, script_path in script_paths.items():
-        process = subprocess.run(["shellcheck", "-e", "SC2148,SC2162", script_path])
-        assert process.returncode == 0, f"`{basename}` failed to compile!"
+@pytest.fixture
+def arguments():
+  return {'tradency': 'bash'}
 
+def test_compilation(script_paths, scripts_to_skip):
+  for basename, script_path in script_paths.items():
+    if basename in scripts_to_skip:
+      print(f"Skipping `{basename}`...")
+      continue
 
-def test_execution(script_paths):
-    for basename, script_path in script_paths.items():
-        process = subprocess.run(
-            ["bash", "-c", "-i",
-                f"source {script_path}; {basename} > /dev/null"]
-        )
-        assert process.returncode == 0, f"`{basename}` failed to execute!"
+    print(f"Compiling `{basename}`...", end='\t')
+
+    process = subprocess.run(["shellcheck", "-e", "SC2148,SC2162", script_path])
+    assert process.returncode == 0, f"`{basename}` failed to compile!"
+    print("OK")
+
+def test_execution(script_paths, scripts_to_skip, arguments):
+  for basename, script_path in script_paths.items():
+    if basename in scripts_to_skip:
+      print(f"Skipping `{basename}`...")
+      continue
+    print(f"Executing `{basename}`...", end='\t')
+
+    process = subprocess.run(
+      [
+        "bash",
+        "-c",
+        "-i",
+        f"source {script_path}; {basename} {arguments.get(basename, '')} 1> /dev/null"
+      ]
+    )
+    assert process.returncode == 0, f"`{basename}` failed to execute!"
+    print("OK")
